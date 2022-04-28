eacebc9505d1633b36c223dc6a0d57708f391401
AneeshRBhat <aneeshbhatr@gmail.com>
2022-04-28 13:33:10 +0530

    script: made the javascript

---
+++ script.js
@@ -0,0 +1,28 @@
+var inputText = document.querySelector("#input-text")
+var btnTranslate = document.querySelector("#btn-translate")
+var outputArea = document.querySelector("#output-area")
+
+// var serverURL = "https://api.funtranslations.com/translate/shakespeare.json"
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+function makeTranslationURL(textInput) {
+    return serverURL + "?text=" + textInput
+}
+
+function errorHandler(error) {
+    console.log("error:" + error)
+    alert("An error has occured with the API, please try again after some time")
+}
+
+function clickHandler() {
+    var textInput = inputText.value 
+
+    fetch(makeTranslationURL(textInput))
+        .then(response => response.json())
+        .then(json => {
+                        var outputText = json.contents.translated
+                        outputArea.innerText = outputText
+        })
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler)
